9fe91b7579ea4074a24af7b77289cad0fc4b2d38
Ahakimay <67209206+Ahakimay@users.noreply.github.com>
2022-05-04 22:41:20 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Nested Arrays. My question is where can I see the output. I do not see the output on the page when I open it with Chrome or live server//
+const numberClusters = [[1,2], [3,4],[5,6]];
+
+const target = numberClusters [2] [1];
+
+console.log(numberClusters);
+
+
+//Arrays. My question is where can I see the output? I do not see  the out put when I open the page with  Chrome or live Server.//
+const hobbies = ['Reading novel', 'Jogging', 'Watching Tv'];
+
+console.log(hobbies)
+
